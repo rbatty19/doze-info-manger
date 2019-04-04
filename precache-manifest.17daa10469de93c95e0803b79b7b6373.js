882af4e3202565d8a6f278ca94ab1ef7655f35ee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-04 12:36:06 +0000

    Deploy Rober19/doze-info-manger to github.com/Rober19/doze-info-manger.git:gh-pages

---
+++ precache-manifest.17daa10469de93c95e0803b79b7b6373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "399fcdbe7514407befbc",
+    "url": "/doze-info-manger/static/css/main.57411b43.chunk.css"
+  },
+  {
+    "revision": "399fcdbe7514407befbc",
+    "url": "/doze-info-manger/static/js/main.11d75d31.chunk.js"
+  },
+  {
+    "revision": "f958a86fbe7b35f61b5a",
+    "url": "/doze-info-manger/static/js/runtime~main.f603ca52.js"
+  },
+  {
+    "revision": "f78b8f4a40e74e8bc8bd",
+    "url": "/doze-info-manger/static/css/2.d7e5575f.chunk.css"
+  },
+  {
+    "revision": "f78b8f4a40e74e8bc8bd",
+    "url": "/doze-info-manger/static/js/2.b83c2956.chunk.js"
+  },
+  {
+    "revision": "9e11e9efd830f12d369b29e7ffa6b696",
+    "url": "/doze-info-manger/index.html"
+  }
+];
